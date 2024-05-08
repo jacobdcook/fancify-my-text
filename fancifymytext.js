0e3eb5d86edc96efe7cdc1ffe7966d96b6b9e0bf
jacobdcook <jacobcookofficial@gmail.com>
2024-05-08 12:01:52 -0700

    Fixing functionality.

--- fancifymytext.js
+++ fancifymytext.js
@@ -1,27 +1,30 @@
-document.getElementById("biggerBtn").onclick = function() {
+function makeBigger() {
     document.getElementById("textArea").style.fontSize = "24pt";
-};
-
-function changeStyle() {
+  }
+  
+  function fancyText() {
     var textArea = document.getElementById("textArea");
-    if (document.getElementById("fancyShmancy").checked) {
-        textArea.style.fontWeight = "bold";
-        textArea.style.color = "blue";
-        textArea.style.textDecoration = "underline";
-    } else if (document.getElementById("boringBetty").checked) {
-        textArea.style.fontWeight = "normal";
-        textArea.style.color = "black";
-        textArea.style.textDecoration = "none";
-    }
-}
-
-document.getElementById("mooBtn").onclick = function() {
+    textArea.style.fontWeight = "bold";
+    textArea.style.color = "blue";
+    textArea.style.textDecoration = "underline";
+  }
+  
+  function boringText() {
     var textArea = document.getElementById("textArea");
-    var sentences = textArea.value.split(".");
+    textArea.style.fontWeight = "normal";
+    textArea.style.color = "black";
+    textArea.style.textDecoration = "none";
+  }
+  
+  function makeUppercase() {
+    var textArea = document.getElementById("textArea");
+    var text = textArea.value;
+    var sentences = text.split(".");
     for (var i = 0; i < sentences.length; i++) {
-        if (sentences[i].trim().length > 0) {
-            sentences[i] = sentences[i].trim() + "-Moo";
-        }
+      var words = sentences[i].trim().split(" ");
+      var lastWord = words[words.length - 1];
+      words[words.length - 1] = lastWord.toUpperCase() + "-Moo";
+      sentences[i] = words.join(" ");
     }
-    textArea.value = sentences.join(". ").toUpperCase();
-};
+    textArea.value = sentences.join(".");
+  }
